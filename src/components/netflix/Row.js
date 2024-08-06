9dd29a11ebc1f341e372e2c7cf189afd5a344a41
hwlee-study <hwlee8042@gmail.com>
2024-08-06 23:37:27 +0900

    영화 리스트, 모달 상세 페이지 추가

--- src/components/netflix/Row.js
+++ src/components/netflix/Row.js
@@ -1,4 +1,65 @@
-function Row() {
-  return <div>row contetns</div>
+import { useEffect, useState } from 'react'
+import axiosInstance from '../../api/axios'
+import './Row.css'
+import MovieModal from './movieModal'
+
+function Row({ title, id, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([])
+  const [modalOpen, setModalOpen] = useState(false)
+  const [movieSelected, setMovieSelected] = useState({})
+
+  const fetchMovieDatas = async () => {
+    const { status, data } = await axiosInstance.get(fetchUrl)
+
+    if (status === 200) {
+      const { results } = data
+      setMovies(results)
+    }
+  }
+
+  useEffect(() => {
+    fetchMovieDatas()
+  }, [])
+
+  const movieHandleClick = (movie) => {
+    setModalOpen(true)
+    setMovieSelected(movie)
+  }
+
+  return (
+    <section className="row">
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span
+            className="arrow"
+            onClick={() => (document.getElementById(id).scrollLeft -= window.innerWidth - 80)}
+          >
+            {'<'}
+          </span>
+        </div>
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+              src={`${process.env.REACT_APP_MOVIE_IMG_ENDPOINT}/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+              alt={movie.name}
+              onClick={() => movieHandleClick(movie)}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span
+            className="arrow"
+            onClick={() => (document.getElementById(id).scrollLeft += window.innerWidth - 80)}
+          >
+            {'>'}
+          </span>
+        </div>
+      </div>
+      {modalOpen && <MovieModal {...movieSelected} setModalOpen={setModalOpen} />}
+    </section>
+  )
 }
 export default Row
---
+++ src/components/netflix/movieModal/index.js
@@ -0,0 +1,40 @@
+import './MovieModal.css'
+
+function MovieModal({
+  backdrop_path,
+  title,
+  overview,
+  name,
+  release_date,
+  first_air_date,
+  vote_average,
+  setModalOpen,
+}) {
+  return (
+    <div className="presentation">
+      <div className="wrapper-modal">
+        <div className="modal">
+          <span className="modal-close" onClick={() => setModalOpen(false)}>
+            X
+          </span>
+          <img
+            className="modal__poster-img"
+            src={`${process.env.REACT_APP_MOVIE_IMG_ENDPOINT}/original/${backdrop_path}`}
+            alt="modal__poster-img"
+          />
+          <div className="modal__content">
+            <p className="modal__details">
+              <span className="modal__user-perc">100% for you</span>
+              {release_date ? release_date : first_air_date}
+            </p>
+            <h2 className="modal__title">{title ? title : name}</h2>
+            <p className="modal__overview">평점 : {vote_average}</p>
+            <p className="modal__overview">{overview}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MovieModal
