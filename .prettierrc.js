05208cfa6b360f482419ce8878373b3dac4f5c31
hwlee-study <hwlee8042@gmail.com>
2024-11-03 20:05:17 +0900

    Updates

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+  arrowParens: 'always',
+}
